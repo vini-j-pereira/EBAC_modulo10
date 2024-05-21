1b7c606116e8d749529be5a4090bf10e439119a1
vini-j-pereira <viniciusjosepereira@gmail.com>
2024-05-21 00:54:02 -0300

    commit_ vadida form jquery

--- EBAC modulo 10/modulo10.js
+++ EBAC modulo 10/modulo10.js
@@ -7,3 +7,43 @@ $(document).ready(function(){
         $('nav').slideToggle();
     })
 })
+
+
+$("#telefone").mask('(00) 00000-0000', {
+    placeholder: '(00) 00000-0000'
+})
+
+$('form').validate({
+    rules: {
+        nome: {
+            required: true
+        },
+        email: {
+            required: true, 
+            email: true
+        },
+        telefone: {
+            required: true
+        },
+        mensagem: {
+            required: true
+        },
+        autoInt: {
+            required: false
+        },
+    },
+    messages: {
+        nome: 'Por favor, insira o caralho do seu nome',
+        telefone: 'Este campo é obrigatorio.',
+        email: 'Este campo é obrigatorio.'
+    },
+    submitHandler: function(form) {
+        console.log(form);
+    },
+    invalidHandler: function(evento, validador) {
+      let camposIncorretos = validador.numberOfInvalids();
+      if(camposIncorretos) {
+        alert(`Existem ${camposIncorretos} campos obrigatorios`)
+      }
+    }
+})
